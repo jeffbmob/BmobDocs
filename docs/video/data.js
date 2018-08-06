9430f9adbf1b9432ccf3ae2d9501cb2ee5abed64
jeffbmob <test@example.com>
2018-08-07 04:06:41 +0800

    服务端自动生成文档

---
+++ docs/video/data.js
@@ -0,0 +1,490 @@
+var data = [
+    {
+        id: 'data',
+        title: '数据服务',
+        data: [{
+            title: 'Bmob简介及crud(增删查改)初体验',
+            chapter: [
+                {
+                    title: 'Bmob数据SDK介绍及集成与初始化',
+                    id: 'XMzEwNDcyNzQ4NA==',
+                    time: '03:41',
+                    bilibili: 'https://www.bilibili.com/video/av16529366'
+                },
+                {
+                    title: '1_1_添加一行数据1',
+                    id: 'XMzEwOTE2MDAxMg==',
+					time: '01:18',
+                    bilibili: 'https://www.bilibili.com/video/av16529431'
+                },
+                {
+                    title: '1_2_添加一行数据2',
+                    id: 'XMzEwOTE4MjA5Mg==',
+					time: '05:48',
+                    bilibili: 'https://www.bilibili.com/video/av16530304'
+                },
+                {
+                    title: '2-1_查询一行数据',
+                    id: 'XMzEwOTI0NDcxNg==',
+					time: '03:12',
+                    bilibili: 'https://www.bilibili.com/video/av16530512'
+                },
+                {
+                    title: '2_2_更新一行数据',
+                    id: 'XMzEwOTIzMTI2MA==',
+					time: '02:07',
+                    bilibili: 'https://www.bilibili.com/video/av16533861'
+                },
+                {
+                    title: '2_3_删除一行数据',
+                    id: 'XMzEwOTI2MDI4NA==',
+                    time: '02:27',
+                    bilibili: 'https://www.bilibili.com/video/av16534573'
+                }
+            ]
+        },
+            {
+                title: 'crud详细使用及数据类型介绍',
+                chapter: [
+                    {
+                        title: '3_1_数据对象与数据类介绍与使用1',
+                        id: 'XMzEwOTI2NjgzMg==',
+						time: '08:00',
+                        bilibili: 'https://www.bilibili.com/video/av16535300'
+                    },
+                    {
+                        title: '3_2_数据对象与数据类介绍与使用2',
+                        id: 'XMzEwOTM3MjU2MA==',
+						time: '01:35',
+                        bilibili: 'https://www.bilibili.com/video/av16535615'
+                    },
+                    {
+                        title: '4_1_自定义表名的使用',
+                        id: 'XMzEwOTM3NTM4OA==',
+						time: '01:57',
+                        bilibili: 'https://www.bilibili.com/video/av16535825'
+                    },
+                    {
+                        title: '4_2_自定义表名下的数据查询',
+                        id: 'XMzEwOTM4ODI2MA==',
+						time: '02:22',
+                        bilibili: 'https://www.bilibili.com/video/av16536090'
+                    },
+                    {
+                        title: '5_插入数据详解',
+                        id: 'XMzEwOTM5ODM0NA==',
+						time: '04:04',
+                        bilibili: 'https://www.bilibili.com/video/av16536485'
+                    },
+                    {
+                        title: '6_更新数据详解之原子计数器',
+                        id: 'XMzEwOTQxMTcwOA==',
+						time: '05:14',
+                        bilibili: 'https://www.bilibili.com/video/av16538722'
+                    },
+                    {
+                        title: '7_更新数据详解之删除字段的值',
+                        id: 'XMzEwOTQzMzI4MA==',
+						time: '02:42',
+                        bilibili: 'https://www.bilibili.com/video/av16539370'
+                    },
+                    {
+                        title: '8_批量操作详解之批量插入数据',
+                        id: 'XMzEwOTQ0NTA0OA==',
+						time: '09:00',
+                        bilibili: '#'
+                    },
+                    {
+                        title: '9_批量操作详解之批量更新数据',
+                        id: 'XMzEwOTY0MTEyMA==',
+						time: '05:03',
+                        bilibili: 'https://www.bilibili.com/video/av16540200'
+                    },
+                    {
+                        title: '10_批量操作详解之同时提交增删改',
+                        id: 'XMzEwOTQ5MTM5Ng==',
+						time: '06:32',
+                        bilibili: 'https://www.bilibili.com/video/av16540675'
+                    },
+                    {
+                        title: '11_数据查询详解之多条数据查询1及debug小技巧',
+                        id: 'XMzEwOTUxMDc2MA==',
+						time: '07:31',
+                        bilibili: 'https://www.bilibili.com/video/av16542086'
+                    },
+                    {
+                        title: '12_1_数据查询详解之条件查询与子查询易错写法',
+                        id: 'XMzEwOTUzMjM2OA==',
+						time: '05:31',
+                        bilibili: 'https://www.bilibili.com/video/av16542312'
+                    },
+                    {
+                        title: '13_数据查询详解之时间查询',
+                        id: 'XMzEwOTU1ODM2NA==',
+						time: '03:41',
+                        bilibili: 'https://www.bilibili.com/video/av16557206'
+                    },
+                    {
+                        title: '14_数据查询详解之分页查询',
+                        id: 'XMzEwOTU5NzQxNg==',
+                        time: '14:13',
+                        bilibili: 'https://www.bilibili.com/video/av16557516'
+                    },
+                    {
+                        title: '15_数据查询详解之排序',
+                        id: 'XMzEwOTYyNDc1Ng==',
+						time: '04:19',
+                        bilibili: 'https://www.bilibili.com/video/av16558601'
+                    },
+                    {
+                        title: '16_数据查询详解之复合查询',
+                        id: 'XMzEwOTY1OTIxMg==',
+                        time: '08:18',
+                        bilibili: 'https://www.bilibili.com/video/av16560326'
+                    },
+                    {
+                        title: '17_数据查询详解之查询结果计数',
+                        id: 'XMzEwOTY1OTIxMg==',
+						time: '08:18',
+                        bilibili: 'https://www.bilibili.com/video/av16560486'
+                    },
+                    {
+                        title: '18_数据查询详解之统计查询',
+                        id: 'XMzEwOTY5NjcwOA==',
+						time: '08:37',
+                        bilibili: 'https://www.bilibili.com/video/av16562131'
+                    },
+                    {
+                        title: '19_数据查询详解之分组统计',
+                        id: 'XMzEwOTc1Njg5Ng==',
+						time: '07:15',
+                        bilibili: 'https://www.bilibili.com/video/av16563470'
+                    },
+                    {
+                        title: '20_数组详解1',
+                        id: 'XMzEwOTc4NzE0MA==',
+						time: '08:57',
+                        bilibili: 'https://www.bilibili.com/video/av16565026'
+                    },
+                    {
+                        title: '21_数组详解1',
+                        id: 'XMzEwOTgyMjExMg==',
+						time: '01:49',
+                        bilibili: 'https://www.bilibili.com/video/av16565021'
+                    },
+                    {
+                        title: '22_数组详解1',
+                        id: 'XMzEwOTgzNzM4OA==',
+                        time: '07:34',
+                        bilibili: 'https://www.bilibili.com/video/av16565320'
+                    }
+                ]
+            },
+            {
+                title: '数据关联详细使用',
+                chapter: [
+                    {
+                        title: '23_1_数据关联详解之添加一对一关联1',
+                        id: 'XMzEwOTg2ODE0MA==',
+                        time: '05:50',
+                        bilibili: 'https://www.bilibili.com/video/av16566190'
+                    },
+                    {
+                        title: '23_2_数据关联详解之添加一对一关联2',
+                        id: 'XMzEwOTg5MzA4NA==',
+						time: '02:30',
+                        bilibili: 'https://www.bilibili.com/video/av16566759'
+                    },
+                    {
+                        title: '24_数据关联详解之查询一对一关联与include',
+                        id: 'XMzEwOTkxNDYzNg==',
+						time: '05:45',
+                        bilibili: 'https://www.bilibili.com/video/av16567083'
+                    },
+                    {
+                        title: '25_数据关联详解之查询一对多关联',
+                        id: 'XMzEwOTkzNDcwMA==',
+						time: '05:46',
+                        bilibili: 'https://www.bilibili.com/video/av16567300'
+                    },
+                    {
+                        title: '26_数据关联详解之添加多对多关联',
+                        id: 'XMzEwOTk2NDUxMg==',
+						time: '07:37',
+                        bilibili: 'https://www.bilibili.com/video/av16567420'
+                    },
+                    {
+                        title: '27_数据关联详解之查询多对多关联',
+                        id: 'XMzEwOTk3MDcwOA==',
+                        time: '04:58',
+                        bilibili: 'https://www.bilibili.com/video/av16567583'
+                    },
+                    {
+                        title: '28_数据关联详解之内部查询',
+                        id: 'XMzEwOTk4ODM5Mg==',
+                        time: '07:32',
+                        bilibili: 'https://www.bilibili.com/video/av16568220'
+                    }
+                ]
+            },
+            {
+                title: '文件相关功能和地理位置',
+                chapter: [
+                    {
+                        title: '29_1_文件上传介绍及上传单一文件常见写法',
+                        id: 'XMzEwOTk5NDMyMA==',
+                        time: '03:11',
+                        bilibili: 'https://www.bilibili.com/video/av16568592'
+                    },
+                    {
+                        title: '29_2_文件上传中的常见动态权限错误展示',
+                        id: 'XMzExMDAwNzQ4OA==',
+                        time: '01:24',
+                        bilibili: 'https://www.bilibili.com/video/av16568672'
+                    },
+                    {
+                        title: '29_3_文件上传之适配动态权限实现',
+                        id: 'XMzExMDAxODI1Ng==',
+						time: '01:22',
+                        bilibili: 'https://www.bilibili.com/video/av16568695'
+                    },
+                    {
+                        title: '30_文件上传之批量文件上传',
+                        id: 'XMzExMDAyMjI4MA==',
+						time: '09:59',
+                        bilibili: 'https://www.bilibili.com/video/av16569081'
+                    },
+                    {
+                        title: '31_文件详解之BmobFile和数据表的关联实现',
+                        id: 'XMzExMTkzNDUxMg==',
+						time: '06:47',
+                        bilibili: 'https://www.bilibili.com/video/av16569377'
+                    },
+                    {
+                        title: '32_1_文件详解之文件下载及错误展示',
+                        id: 'XMzExMDA1NDcwOA==',
+						time: '05:50',
+                        bilibili: 'https://www.bilibili.com/video/av16570075'
+                    },
+                    {
+                        title: '32_2_文件详解之文件下载的路径错误修正',
+                        id: 'XMzExMDA3MTA4MA==',
+						time: '00:51',
+                        bilibili: 'https://www.bilibili.com/video/av16570137'
+                    },
+                    {
+                        title: '33_文件详解之删除文件',
+                        id: 'XMzExMDA3NDEyMA==',
+                        time: '05:40',
+                        bilibili: 'https://www.bilibili.com/video/av16570397'
+                    },
+                    {
+                        title: '34_地理位置相关功能详解',
+                        id: 'XMzExMDA4NDEzNg==',
+                        time: '10:31',
+                        bilibili: 'https://www.bilibili.com/video/av16571118'
+                    }
+                ]
+            }
+        ]
+    }, {
+        id: 'push',
+        title: '推送服务',
+        data: [{
+            title: '第一节',
+            chapter: [{
+                title: 'BmobPush SDK的简介和集成',
+                id: 'XMzExMTY4ODg2MA==',
+				time: '08:43',
+                bilibili: 'https://www.bilibili.com/video/av16614795'
+            }]
+        }, {
+            title: '第二节',
+            chapter: [{
+                title: '如何在控制台推送消息给客户端',
+                id: 'XMzExMTcyNDY0MA==',
+				time: '06:01',
+                bilibili: 'https://www.bilibili.com/video/av16615668'
+            }]
+        }, {
+            title: '第三节',
+            chapter: [{
+                title: '消息推送的设备信息：Installation',
+                id: 'XMzExMTc0OTE1Mg==',
+				time: '08:24',
+                bilibili: 'https://www.bilibili.com/video/av16617862'
+            }]
+        }, {
+            title: '第四节',
+            chapter: [{
+                title: '推送频道的订阅和退订',
+                id: 'XMzExMTg2MzY0OA==',
+				time: '06:45',
+                bilibili: 'https://www.bilibili.com/video/av16618569'
+            }]
+        }, {
+            title: '第五节',
+            chapter: [{
+                title: '客户端发起的消息推送',
+                id: 'XMzExMTc4NjQzMg==',
+                time: '13:32',
+                bilibili: 'https://www.bilibili.com/video/av16620182'
+            }]
+        }]
+    }, {
+        id: 'im',
+        title: '即时通讯',
+        data: [{
+            title: '第一节',
+            chapter: [{
+                title: 'BmobNewIM SDK 介绍',
+                id: 'XMzExMTA3NDQ0NA==',
+				time: '02:38',
+                bilibili: 'https://www.bilibili.com/video/av16584045'
+            }]
+        }, {
+            title: '第二节',
+            chapter: [{
+                title: 'BmobNewIM SDK 集成',
+                id: 'XMzExMTA3NzMzMg==',
+                time: '10:53',
+                bilibili: 'https://www.bilibili.com/video/av16584352'
+            }]
+        }, {
+            title: '第三节 BmobNewIM SDK 使用',
+            chapter: [{
+                title: '3.1、BmobNewIM 案例流程图',
+                id: 'XMzExMTA5ODgwNA==',
+				time: '04:21',
+                bilibili: 'https://www.bilibili.com/video/av16584563'
+            }, {
+                title: '3.2.1、Bmob的用户系统',
+                id: 'XMzExMTExNzIxMg==',
+				time: '03:18',
+                bilibili: 'https://www.bilibili.com/video/av16584740'
+            }, {
+                title: '3.2.2、IM的本地用户信息',
+                id: 'XMzExMTEyNTc4NA==',
+				time: '03:52',
+                bilibili: 'https://www.bilibili.com/video/av16584858'
+            }, {
+                title: '3.3、IM服务器的连接',
+                id: 'XMzExMTEzOTkyOA==',
+				time: '03:11',
+                bilibili: 'https://www.bilibili.com/video/av16584969'
+            }, {
+                title: '3.4.1、创建IM的会话',
+                id: 'XMzExMTE1NDM2NA==',
+				time: '08:50',
+                bilibili: 'https://www.bilibili.com/video/av16589314'
+            }, {
+                title: '3.4.2、IM会话其他操作',
+                id: 'XMzExMTE5NTE2OA==',
+				time: '08:56',
+                bilibili: 'https://www.bilibili.com/video/av16592101'
+            }, {
+                title: '3.5、IM的消息类型和管理',
+                id: 'XMzExMTI1NTEwMA==',
+				time: '10:59',
+                bilibili: 'https://www.bilibili.com/video/av16594006'
+            }, {
+                title: '3.6、IM的消息发送',
+                id: 'XMzExMTQyMzI4MA==',
+				time: '12:14',
+                bilibili: 'https://www.bilibili.com/video/av16596469'
+            }, {
+                title: '3.7、IM的自定义消息',
+                id: 'XMzExMTUyMjk2OA==',
+				time: '04:44',
+                bilibili: 'https://www.bilibili.com/video/av16611145'
+            }, {
+                title: '3.8、IM的消息接收',
+                id: 'XMzExMTU0NTEwOA==',
+                time: '08:22',
+                bilibili: 'https://www.bilibili.com/video/av16612035'
+            }, {
+                title: '3.9、好友管理',
+                id: 'XMzExMTU3ODI5Mg==',
+                time: '15:29',
+                bilibili: 'https://www.bilibili.com/video/av16613633'
+            }]
+        }, {
+            title: '第四节',
+            chapter: [{
+                title: 'IM SDK 的混淆和旧版回顾',
+                id: 'XMzExMTY4MzI2NA==',
+                time: '01:56',
+                bilibili: 'https://www.bilibili.com/video/av16613823'
+            }]
+        }]
+    }, {
+        id: 'weChat',
+        title: '小程序',
+        data: [{
+            title: '第一节',
+            chapter: [{
+                title: '小程序开发基础',
+                detail: '注册成为微信公众平台开发者</br>安装开发者工具</br>小程序开发工具介绍</br>&nbsp&nbsp&nbsp&nbsp目录介绍</br>&nbsp&nbsp&nbsp&nbsp项目app.js相关目录</br>&nbsp&nbsp&nbsp&nbsp文件介绍</br>&nbsp&nbsp&nbsp&nbsp逻辑层</br>&nbsp&nbsp&nbsp&nbsp视图层</br>&nbsp&nbsp&nbsp&nbsp数据层</br>学习技能</br>&nbsp&nbsp&nbsp&nbspDIV+CSS</br>&nbsp&nbsp&nbsp&nbspJavaScript</br>&nbsp&nbsp&nbsp&nbsp小程序文档</br>',
+                id: 'XMzI4NzA0NjEwOA==',
+                time: '13:50',
+                bilibili: 'https://www.bilibili.com/video/av17914778'
+            }]
+        }, {
+            title: '第二节',
+            chapter: [{
+                title: '小程序后端介绍',
+                detail: 'Bmob 文档<br/>Bmob示例介绍<br/>增删改查<br/>模块功能<br/>项目实战一：准备工作<br/>相关配置<br/>&nbsp;&nbsp;&nbsp;&nbsp;小程序appid<br/>下载前端UI<br/>Bmob控制台介绍<br/>&nbsp;&nbsp;&nbsp;&nbsp;云函数介绍<br/>&nbsp;&nbsp;&nbsp;&nbsp;支付介绍<br/>数据库设计<br/>测试后端<br/>&nbsp;&nbsp;&nbsp;&nbsp;取出数据<br/>',
+                id: 'XMzI4NzA2MDI0MA==',
+                time: '13:27',
+                bilibili: 'https://www.bilibili.com/video/av17914995'
+            }]
+        }, {
+            title: '第三节',
+            chapter: [{
+                title: '相关准备工作',
+                detail: '小程序相关配置<br/>UI代码准备<br/>后端使用介绍<br/>数据库设计<br/>测试与后端连接<br/>',
+                id: 'XMzI4NzA3NDM1Mg==',
+                time: '26:49',
+                bilibili: 'https://www.bilibili.com/video/av17915146'
+            }]
+        }, {
+            title: '第四节',
+            chapter: [{
+                title: '实战一：景区首页',
+				detail: '轮播图<br/>最近加入<br/>数据导出导入<br/>数据排序<br/>',
+                id: 'XMzI4NzEwMTA1Mg==',
+                time: '14:24',
+                bilibili: 'https://www.bilibili.com/video/av17915113'
+            }]
+        }, {
+            title: '第五节',
+            chapter: [{
+                title: '实战一：景区列表、详情',
+				detail: '景区列表<br/>景区信息<br/>评论<br/>',
+                id: 'XMzI5MDAwOTA3Mg==',
+                time: '22:14',
+                bilibili: 'https://www.bilibili.com/video/av17947666'
+            }]
+        }, {
+            title: '第六节',
+            chapter: [{
+                title: '实战一：评论功能',
+				detail: '关联表查询<br/>添加评论<br/>显示评论<br/>发布上线<br/>',
+                id: 'XMzMwMzY3NzkyOA==',
+                time: '22:55',
+                bilibili: 'https://www.bilibili.com/video/av18130132'
+            }]
+        }]
+    }, {
+        id: 'cloudFunction',
+        title: '云函数',
+        data: [{
+            title: '第一节',
+            chapter: [{
+                title: '公众号开发',
+                id: 'XMzMwNjI2OTE5Ng==',
+                time: '22:36',
+                bilibili: 'https://www.bilibili.com/video/av18159673'
+            }]
+        }]
+    }];
